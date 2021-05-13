d4e956e205de85a76110e1db38f2736c8f074217
welcome <welcome@DESKTOP-LIIV73I>
2021-05-13 19:38:22 +0530

    Added Repository

---
+++ src/main/java/com/hsm/healthservicemanagement/repository/IDietRepository.java
@@ -0,0 +1,14 @@
+package com.hsm.healthservicemanagement.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hsm.healthservicemanagement.entity.Diet;
+
+//Spring Data JPA
+
+@Repository
+public interface IDietRepository extends JpaRepository<Diet, Integer>{
+}
